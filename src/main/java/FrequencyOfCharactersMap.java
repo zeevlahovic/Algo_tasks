bfe1d62711fac6c97e9a7874cfbd0e2e3e9bcd7d
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-02-22 12:08:28 -0500

    first commit

--- src/main/java/FrequencyOfCharactersMap.java
+++ src/main/java/FrequencyOfCharactersMap.java
@@ -42,3 +42,21 @@ public class FrequencyOfCharactersMap {
     }
 }
 
+/*Write a method that returns the frequency of each character of a given String parameter.
+ If the given String is null, then return null
+ If the given String is empty return an empty map
+Example-1
+input: “”
+output: empty map
+explanation: input is empty
+Example-2
+input: null
+output: null
+explanation: input is null.
+Example-3
+input: responsible
+output: {r=1, e=2, s=2, p=1, o=1, n=1, i=1, b=1, l=1}
+explanation: characters are keys and occurrences are values
+
+ */
+
