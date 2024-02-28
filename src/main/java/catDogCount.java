912f1e1e5fdd1f6196c5760b86e629d9fc555327
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-02-27 21:31:12 -0500

    CatDog

--- src/main/java/catDogCount.java
+++ src/main/java/catDogCount.java
@@ -13,3 +13,12 @@ public class catDogCount {
         return countCat == countDog;
     }
 }
+
+/*Return true if the string "cat" and "dog" appear the same number of times in the given string.
+
+
+catDog("catdog") → true
+catDog("catcat") → false
+catDog("1cat1cadodog") → true
+
+ */
