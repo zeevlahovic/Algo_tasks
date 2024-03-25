4f347b53f8617d1341bed1e41d2e85e58b145c85
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-03-24 23:43:42 -0400

    ValidPalindrome

--- src/main/java/ValidPalindrome.java
+++ src/main/java/ValidPalindrome.java
@@ -25,6 +25,22 @@ public class ValidPalindrome {
         return false;
     }
 
+    public static boolean isPalindrome(String s) {
+
+        s = s.toLowerCase();
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
 
     public static void main(String[] args) {
 
