dcf3456c2cee94e75c5493f00d7b60c490818167
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-03-29 23:28:47 -0400

    TwoSum

--- src/main/java/TwoSumTask.java
+++ src/main/java/TwoSumTask.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSumTask {
 
@@ -18,6 +20,19 @@ public class TwoSumTask {
             }
         }
 
+        return new int[]{};
+    }
+
+    public static int[] twoSumOptimalSolution(int[] array, int targetValue){
+        // Complexity of the solution is O(n) but I have space complexity of O(n)
+        // create hashmap
+        Map<Integer,Integer> map=new HashMap<>();
+        for (int i = 0; i <array.length ; i++) {
+            int potentialMatch=targetValue-array[i];
+            if (map.containsKey(potentialMatch)) return new int[] {i,map.get(potentialMatch)};
+            else map.put(array[i], i);
+
+        }
         return new int[]{};
     }
 
